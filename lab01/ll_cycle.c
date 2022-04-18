9a49933408a58045e643d7cc730f16b0200e5281
shejialuo <shejialuo@gmail.com>
2022-04-18 15:32:01 +0800

    Finish Lab 1

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if(head == NULL) return 0;
+    node* hare = head ;
+    node* tortoise = head;
+    while(hare) {
+      if(hare->next == NULL)
+        return 0;
+      hare = hare->next;
+      hare = hare->next;
+      tortoise = tortoise->next;
+      if(hare == tortoise) {
+        return 1;
+      }
+    }
     return 0;
 }
