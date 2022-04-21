a8284130bb23867b80c12e6b188ba6cd5159511a
shejialuo <shejialuo@gmail.com>
2022-04-21 17:49:39 +0800

    Finish project 2 argmax.s and relu.s

--- project2/src/argmax.s
+++ project2/src/argmax.s
@@ -6,36 +6,44 @@
 #	element. If there are multiple, return the one
 #	with the smallest index.
 # Arguments:
-# 	a0 (int*) is the pointer to the start of the vector
+# a0 (int*) is the pointer to the start of the vector
 #	a1 (int)  is the # of elements in the vector
 # Returns:
 #	a0 (int)  is the first index of the largest element
 #
-# If the length of the vector is less than 1, 
+# If the length of the vector is less than 1,
 # this function exits with error code 7.
 # =================================================================
 argmax:
-
-    # Prologue
-
-
 loop_start:
-
-
-
-
-
-
-
+    add t0, x0, x0
+    addi t0, x0, 1
+    blt a1, t0, loop_error
+    # t0 is used as the current length
+    add t0, x0, x0
+    # t1 is used as the offset
+    add t1, x0, x0
+    add t1, x0, a0
+    # t2 is used as the index stored
+    add t2, x0, x0
+    # t3 is used as the minimum
+    lw t3, 0(t1)
 
 loop_continue:
-
-
+    lw t4, 0(t1)
+    bge t3, t4, greater_equal_min
+    add t2, t0, x0
+    add t3, t4, x0
+  greater_equal_min:
+    addi t0, t0, 1
+    addi t1, t1, 4
+    blt t0, a1, loop_continue
 
 loop_end:
-    
+    add a0, t2, x0
+    ret
 
-    # Epilogue
-
-
+loop_error:
+    add a0, x0, x0
+    addi a0, x0, 7
     ret
--- project2/src/relu.s
+++ project2/src/relu.s
@@ -4,35 +4,40 @@
 # ==============================================================================
 # FUNCTION: Performs an inplace element-wise ReLU on an array of ints
 # Arguments:
-# 	a0 (int*) is the pointer to the array
+# a0 (int*) is the pointer to the array
 #	a1 (int)  is the # of elements in the array
 # Returns:
 #	None
 #
-# If the length of the vector is less than 1, 
+# If the length of the vector is less than 1,
 # this function exits with error code 8.
 # ==============================================================================
 relu:
-    # Prologue
-
-
 loop_start:
-    
 
+    add t0, x0, x0
+    addi t0, x0, 1
+    blt a1, t0, loop_error
 
-
-
-
+    # t0 is used as the current length
+    add t0, x0, x0
+    # t1 is used as the offset
+    add t1, x0, x0
+    add t1, x0, a0
 
 
 loop_continue:
-
-
-
+    lw t2, 0(t1)
+    bge t2, x0, great_than_0
+    add t3, x0, x0
+    sw t3, 0(t1)
+  great_than_0:
+    addi t0, t0, 1
+    addi t1, t1, 4
+    blt t0, a1, loop_continue
 loop_end:
-
-
-    # Epilogue
-
-    
-	ret
+	  ret
+loop_error:
+    add a0, x0, x0
+    addi a0, x0, 8
+    ret
