bf1a3e03eb8e5d9a38a9549994a963bf3b967748
shejialuo <shejialuo@gmail.com>
2022-05-04 00:23:22 +0800

    Finish lab 4 part I

--- lab04/megalistmanips.s
+++ lab04/megalistmanips.s
@@ -61,21 +61,41 @@ map:
     # - 4 for the array pointer
     # - 4 for the size of the array
     # - 4 more for the pointer to the next node
-mapLoop:
-    add t1, s0, x0      # load the address of the array of current node into t1
-    lw t2, 4(s0)        # load the size of the node's array into t2
 
-    add t1, t1, t0      # offset the array address by the count
+    # fix error
+    lw t1, 0(s0)        # load the address of the array of current node into t1
+    lw t2, 4(s0)        # load the size of the node's array into t2
+mapLoop:
+
     lw a0, 0(t1)        # load the value at that address into a0
+
+    # fix error, should save the temporary
+    addi sp, sp, -12
+    sw t0, 0(sp)
+    sw t1, 4(sp)
+    sw t2, 8(sp)
 
     jalr s1             # call the function on that value.
 
+    # fix error, shoud restore the temporary
+    lw t0, 0(sp)
+    lw t1, 4(sp)
+    lw t2, 8(sp)
+    addi sp, sp, 12
+
     sw a0, 0(t1)        # store the returned value back into the array
     addi t0, t0, 1      # increment the count
+
+    # fix error, offset should be 4
+    addi t1, t1, 4
+
     bne t0, t2, mapLoop # repeat if we haven't reached the array size yet
 
-    la a0, 8(s0)        # load the address of the next node into a0
-    lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
+    # fix error, should load word
+    lw a0, 8(s0)        # load the address of the next node into a0
+
+    # fix error, should just assign
+    add a1, s1, x0        # put the address of the function back into a1 to prepare for the recursion
 
     jal  map            # recurse
 done:
